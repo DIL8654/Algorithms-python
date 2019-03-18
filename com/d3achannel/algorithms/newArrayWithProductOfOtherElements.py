0ff6c12bf95746765ba3d5c56fb93f7245248581
Dilanka Muthukumarana <dilanka85@gmail.com>
2019-03-18 12:59:02 +0530

    newArrayWithProductOfOtherElements.py added linear solution without division

--- com/d3achannel/algorithms/newArrayWithProductOfOtherElements.py
+++ com/d3achannel/algorithms/newArrayWithProductOfOtherElements.py
@@ -1,5 +1,5 @@
-import  time;
-from functools import reduce ;
+import time;
+from functools import reduce;
 import numpy;
 X = [1,2,3,4,5];
 
@@ -70,3 +70,74 @@ def calculateNewArray3(x):
     return n;
 
 print(calculateNewArray3(X));
+
+
+"""
+
+Optimized without Divition   and with upper boud product and lowerbound product
+
+complexity is O(n) here space complexity is O(n)
+
+"""
+
+def calculateNewArray4(x):
+
+    start = time.time();
+
+    upperboundProduct = [1 for _ in range(len(x))];
+    lowerboundProduct = [1 for _ in range(len(x))];
+    finalProduct = [1 for _ in range(len(x))];
+    temp = 1;
+    for i in range(len(x)):
+        upperboundProduct[i] *= temp;
+        temp*=x[i];
+
+    temp = 1;
+
+    for j in reversed(range(len(x))):
+        lowerboundProduct[j] *= temp;
+        temp *= x[j];
+
+
+    for p in range(len(x)):
+        finalProduct[p] = lowerboundProduct[p]*upperboundProduct[p];
+
+    end = time.time()
+    print("time for calculateNewArray4 in milliseconds ", (end - start) * 1000);
+
+    return finalProduct;
+
+print(calculateNewArray4(X));
+
+
+"""
+
+Optimized without Divition and space complexity   and with upper boud product and lowerbound product
+
+complexity is O(n) here space complexity is O(1)
+
+"""
+
+def calculateNewArray5(x):
+
+    start = time.time();
+
+    upperboundProduct = [1 for _ in range(len(x))];
+    finalProduct = [1 for _ in range(len(x))];
+    temp = 1;
+    for i in range(len(x)):
+        upperboundProduct[i] *= temp;
+        temp*=x[i];
+
+    temp = 1;
+
+    for j in reversed(range(len(x))):
+        upperboundProduct[j] *= temp;
+        temp *= x[j];
+
+    end = time.time()
+    print("time for calculateNewArray5 in milliseconds ", (end - start) * 1000);
+
+    return upperboundProduct;
+
+print(calculateNewArray5(X));
